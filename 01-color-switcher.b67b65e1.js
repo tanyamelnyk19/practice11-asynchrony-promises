94d749a6327a44490b68d869aac5da200c821ef4
tanyamelnyk19 <79224689+tanyamelnyk19@users.noreply.github.com>
2024-05-19 20:44:42 +0000

    Deploying to gh-pages from @ tanyamelnyk19/practice11-asynchrony-promises@9f649855f7996afe8f2e0b4131c7e31de4bcfcc3 🚀

---
+++ 01-color-switcher.b67b65e1.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",function(){n=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3),t.startBtn.disabled=!0}),t.stopBtn.addEventListener("click",function(){clearInterval(n),t.startBtn.disabled=!1});let n=null;
+//# sourceMappingURL=01-color-switcher.b67b65e1.js.map
